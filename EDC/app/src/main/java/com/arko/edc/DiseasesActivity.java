3a36a1d831ac286e424c05a7512451532520706b
Arek Ciarach <arkadiusz1230@gmail.com>
2020-12-15 22:40:47 +0100

    Add menu activity

---
+++ EDC/app/src/main/java/com/arko/edc/DiseasesActivity.java
@@ -0,0 +1,14 @@
+package com.arko.edc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DiseasesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_diseases_activty);
+    }
+}
--- EDC/app/src/main/java/com/arko/edc/MainActivity.java
+++ EDC/app/src/main/java/com/arko/edc/MainActivity.java
@@ -2,7 +2,9 @@ package com.arko.edc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
@@ -20,15 +22,73 @@ public class MainActivity extends AppCompatActivity {
 
 
         initWidgets();
+        setOnCilickListeners();
 
 
     }
 
+    private void setOnCilickListeners(){
+        btnUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, UserInterfaceActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnMeasurement.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MeasurementActivity.class );
+                startActivity(intent);
+            }
+        });
+
+        btnDiseases.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, DiseasesActivity.class );
+                startActivity(intent);
+            }
+        });
+
+        btnPains.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, PainActivity.class );
+                startActivity(intent);
+            }
+        });
+
+
+        btnHistory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, HistoryActivity.class );
+                startActivity(intent);
+            }
+        });
+
+        btnNotes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, NotesActivity.class );
+                startActivity(intent);
+            }
+        });
+
+    }
+
+
     private void initWidgets(){
 
         btnUser = (Button) findViewById(R.id.btnUser);
-
-
+        btnMeasurement = (Button) findViewById(R.id.btnMeasurement);
+        btnDiseases = (Button) findViewById(R.id.btnDiseases);
+        btnPains = (Button) findViewById(R.id.btnPains);
+        btnHistory = (Button) findViewById(R.id.btnHistory);
+        btnNotes = (Button) findViewById(R.id.btnNotes);
 
     }
 
